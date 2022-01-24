f755ab59b74d1f62a03bfd46d18d710d077d64a1
jdonuru1 <98360213+jdonuru1@users.noreply.github.com>
2022-01-24 16:04:36 -0600

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -69,24 +69,3 @@ public class Demo {
 
 	}
 }
-
-class Data {
-	private int x;
-	private int y;
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-}
